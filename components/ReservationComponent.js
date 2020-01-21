14a732b264e0aa0110ac65a6e0acbee4738faa61
Zack McKenna <zackmckenna@gmail.com>
2020-01-21 17:38:17 -0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, Alert, StyleSheet, Picker, Switch, Button, Modal } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import  * as Animatable from 'react-native-animatable'
+import * as Permissions from 'expo-permissions'
+import { Notifications } from 'expo'
 
 class Reservation extends Component {
 
@@ -12,8 +14,31 @@ class Reservation extends Component {
             campers: 1,
             hikeIn: false,
             date: '',
-            showModal: false
+            showModal: false,
+
         };
+    }
+
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
     }
 
     static navigationOptions = {
@@ -130,8 +155,11 @@ class Reservation extends Component {
                                   },
                                   {
                                     text: 'OK',
-                                    onPress: () => console.log('Ok')
-                                  }
+                                    onPress: () => {
+                                        this.presentLocalNotification(this.state.date);
+                                        this.resetForm();
+                                    }
+                                }
                                 ],
                                 { cancelable: false }
                               )}}
